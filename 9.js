0bb10464b24051245e842996052f74ce7eb12634
Sameer Shaikh <shaikhs975@gmail.com>
2023-12-29 23:52:42 +0530

    more logical code of the same problem

--- 9.js
+++ 9.js
@@ -1,3 +1,17 @@
+// var isPalindrome = function (x) {
+//   return x == [...x.toString()].reverse().join("");
+// };
+
 var isPalindrome = function (x) {
-  return x == [...x.toString()].reverse().join("");
+  let numToStr = `${x}`;
+  let len = numToStr.length;
+  let revcount = len;
+  let revStr = "";
+  for (let i = 0; i < len; i++) {
+    revStr += numToStr[--revcount];
+  }
+  return revStr == numToStr;
 };
+
+console.log(isPalindrome(-1234));
+console.log(isPalindrome(1221));
