93c06c04676a2a53e6d26e7e8c226c36bae89a6e
Sameer Shaikh <shaikhs975@gmail.com>
2024-01-05 01:00:14 +0530

    created efficient solution

--- 1470.js
+++ 1470.js
@@ -1,12 +1,19 @@
 /// 1470. Shuffle the Array
 let newArr = [];
 let nums = [2, 5, 1, 3, 4, 7];
+// var shuffle = function (nums, n) {
+//   let res = [];
+//   for (let i = 0; i < n; i++) {
+//     res.push(nums[i], nums[n + i]);
+//   }
+//   return res;
+// };
 var shuffle = function (nums, n) {
-  let res = [];
-  for (let i = 0; i < n; i++) {
-    res.push(nums[i], nums[n + i]);
+  if (nums.length > n) {
+    return [nums[0], nums[n], ...shuffle(nums.slice(1), n)];
+  } else {
+    return [];
   }
-  return res;
 };
 
-shuffle(nums, 3);
+console.log(shuffle(nums, 3));
