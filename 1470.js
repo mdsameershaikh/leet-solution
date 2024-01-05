e3081410168c05edcd574d4f6d2333019d80d781
Sameer Shaikh <shaikhs975@gmail.com>
2024-01-05 11:31:53 +0530

    commented ineffecient code

--- 1470.js
+++ 1470.js
@@ -1,19 +1,19 @@
 /// 1470. Shuffle the Array
 let newArr = [];
 let nums = [2, 5, 1, 3, 4, 7];
-// var shuffle = function (nums, n) {
-//   let res = [];
-//   for (let i = 0; i < n; i++) {
-//     res.push(nums[i], nums[n + i]);
-//   }
-//   return res;
-// };
 var shuffle = function (nums, n) {
-  if (nums.length > n) {
-    return [nums[0], nums[n], ...shuffle(nums.slice(1), n)];
-  } else {
-    return [];
+  let res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(nums[i], nums[n + i]);
   }
+  return res;
 };
+// var shuffle = function (nums, n) {
+//   if (nums.length > n) {
+//     return [nums[0], nums[n], ...shuffle(nums.slice(1), n)];
+//   } else {
+//     return [];
+//   }
+// };
 
 console.log(shuffle(nums, 3));
