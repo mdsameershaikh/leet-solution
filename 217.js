4f28363ba3edc3cf16717f6f7a7b69c45b50ffde
Sameer Shaikh <shaikhs975@gmail.com>
2024-01-09 21:10:09 +0530

    solved this problem with 3 diffrent approaches

---
+++ 217.js
@@ -0,0 +1,33 @@
+var containsDuplicate = function (nums) {
+  let newArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (newArr.includes(nums[i])) {
+      return true;
+    } else {
+      newArr.push(nums[i]);
+    }
+  }
+  return false;
+};
+
+var containsDuplicate = function (nums) {
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (obj.hasOwnProperty(nums[i])) {
+      return true;
+    } else {
+      obj[nums[i]] = nums[i];
+    }
+  }
+  return false;
+};
+
+var containsDuplicate = function (nums) {
+  nums = nums.sort((a, b) => {
+    return a - b;
+  });
+  return nums.some((el, i) => {
+    return el == nums[i + 1];
+  });
+};
+console.log(containsDuplicate([1, 2, 3]));
