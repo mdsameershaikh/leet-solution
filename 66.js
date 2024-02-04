b2bc024a21a8c4c96575b4261b2dd910241c7030
Sameer Shaikh <shaikhs975@gmail.com>
2024-02-04 19:42:52 +0530

    solved 66 no porblem

---
+++ 66.js
@@ -0,0 +1,14 @@
+/**
+ * You are given a large integer represented as an integer array digits,
+ *  where each digits[i] is the ith digit of the integer. The digits are ordered
+ * from most significant to least significant in left-to-right order. The large integer
+ * does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+ */
+var plusOne = function (digits) {
+  let numAddition = BigInt(digits.join(""));
+
+  return numAddition.toString().split("");
+};
+
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
